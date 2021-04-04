840d60f80bde343ba627c3964c6e8249bb209786
Tanzeel U Rehman <tanzeel@dynamiconlinetechnologies.com>
2021-04-04 17:00:11 +0500

    adding road map

---
+++ src/components/Home/RoadMap/RoadMap.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './RoadMap.scss';
+
+function RoadMap() {
+    return (
+        <div className="roadmap-main">
+            <div class="section">
+                <div class="wrapper">
+                    <div class="intro margin-bottom" id="roadmap">
+                        <div style={{opacity: 1, transform: 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)', 'transform-style': 'preserve-3d'}} class="title">Roadmap</div>
+                        <h2 style={{opacity: 1, transform: 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)', 'transform-style': 'preserve-3d'}} class="heading-margin-big">2021 Product and Partnership Roadmap </h2>
+                        <p> </p>
+                    </div><img src="https://i.imgur.com/Nj2XciD.png" loading="lazy" width="1119" sizes="(max-width: 479px) 94vw, (max-width: 1279px) 95vw, 1119px" alt="" class="image-2" style={{opacity: 1, transform: 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)', 'transform-style': 'preserve-3d'}} /></div>
+                <a href="https://bondlyfinance.medium.com/bondlys-q2-2021-roadmap-e9219139a9f3" target="_blank" class="button w-button">Q2 Roadmap</a>
+            </div>
+        </div>
+    )
+}
+
+export default RoadMap
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Header from '../shared/Header/Header'
 import Banner from './Banner/Banner';
 import './index.scss'
+import RoadMap from './RoadMap/RoadMap';
 export default function index() {
     return (
         <div className="home">
                <Banner/>
+               <RoadMap/>
         </div>
     )
 }
