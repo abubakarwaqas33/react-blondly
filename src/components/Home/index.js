ff940aef651b37115168a780bdbc27613e03d716
abubakarchwaqas33 <abubakarch.7033@gmail.com>
2021-04-04 16:57:42 +0500

    banner setting

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from '../shared/Header/Header'
 import Banner from './Banner/Banner';
+import Features from './Features/Features';
+
 import './index.scss'
 export default function index() {
     return (
         <div className="home">
                <Banner/>
+               <Features/>
         </div>
     )
 }
